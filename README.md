0edef0e34caf841b3a61346a6b379cd929509f7c
birdguan <xuchen_guan@163.com>
2020-06-18 17:43:11 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WHICH FILM，探寻您与一部电影的缘分
+![](imgs/show.png)
+
---
+++ app/src/main/java/com/birdguan/whichmovie/base/BaseActivity.java
@@ -0,0 +1,8 @@
+package com.birdguan.whichmovie.base;
+
+/**
+ * @Author: birdguan
+ * @Date: 2020/6/18 16:14
+ */
+public class BaseActivity extends AppCompatActivity {
+}
